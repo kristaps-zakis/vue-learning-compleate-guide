44bdaca39f88782e2a36a287409f8e216e9214f1
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-02-18 12:01:30 +0200

    adding custom params and default event from template function

--- project-3/app.js
+++ project-3/app.js
@@ -2,9 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      userName: '',
     };
   },
   methods: {
+    setName(event, lastName) {
+      /*
+      by default passes the event object, if not said different
+      if need pass default event, adding $event in template function with extra parameters
+      */
+      this.userName = event.target.value + ' ' + lastName;
+    },
     addCount(num) {
       this.counter += num;
     },
