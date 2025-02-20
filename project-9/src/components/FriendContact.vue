0759e26a65f73b6d9a304362f79c531e896bf27f
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-02-20 23:37:12 +0200

    first component creationg

---
+++ project-9/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetailsVisibility">{{detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                    id: 'manuel',
+                    name: "Kristaps Zakis",
+                    phone: "asdasd",
+                    email: "asd@asd.dd"
+                },
+        };
+    },
+    methods: {
+        toggleDetailsVisibility () {
+            this.detailsAreVisible = !this.detailsAreVisible
+        }
+    }
+}
+</script>
