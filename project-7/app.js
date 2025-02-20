dc355d42f6c9029df75f6e446152f982ff8b312c
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-02-20 12:41:20 +0200

    adding vanilla proxy example with console logs

--- project-7/app.js
+++ project-7/app.js
@@ -16,3 +16,30 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+
+const data = {
+  message: 'Vue is great!',
+  longMesage: 'This is longer message',
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMesage = value + ' World';
+    }
+
+    target.message = value;
+
+    console.log(target);
+    console.log(key);
+    console.log(value);
+    // target[key] = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
+proxy.message = 'Vue is great! x2';
+
+console.log(proxy.message);
+console.log(proxy.longMesage);
