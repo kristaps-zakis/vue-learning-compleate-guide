5bb6ebefcafd47445b606be0ce67270d4f03c446
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-02-20 15:25:07 +0200

    creating first component

--- project-8/app.js
+++ project-8/app.js
@@ -19,4 +19,33 @@ const app = Vue.createApp({
   },
 });
 
+app.component('friend-content', {
+  template: `
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">Show Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>`,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'julie',
+        name: 'Julie Jones',
+        phone: '0987 6543 210',
+        email: 'G7OyG@example.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+      console.log(this.detailsAreVisible);
+    },
+  },
+});
+
 app.mount('#app');
