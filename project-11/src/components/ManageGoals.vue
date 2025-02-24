d72a0459b8f647055047c3151f3fb1d0daf59a6c
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-02-24 14:32:03 +0200

    showing patter to show dialog with slot content if input is empty

--- project-11/src/components/ManageGoals.vue
+++ project-11/src/components/ManageGoals.vue
@@ -1,7 +1,39 @@
 <template>
     <div>
         <h2>Manage Goals</h2>
-        <input type="text" />
+        <input type="text" ref="goal"/>
+        <button @click="setGoal">Set Goal</button>
+        <error-alert v-if="inputIsInvalid">
+            <h2>Input is Invalid (empty)</h2>
+            <p>Please enter some goal...</p>
+            <button @click="confirmError">Okay</button>
+        </error-alert>
     </div>
-    
+     
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+
+export default {
+    components: {ErrorAlert},
+    data() {
+        return {
+            inputIsInvalid: false,
+        }
+    },
+    methods: {
+        setGoal() {
+            const enteredValue = this.$refs.goal.value;
+
+            if (enteredValue === "") {
+                // alert("Input is empty")
+                this.inputIsInvalid = true;
+            }
+        },
+        confirmError() {
+            this.inputIsInvalid = false;
+        }
+    }
+}
+</script>
