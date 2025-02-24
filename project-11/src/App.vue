6c99ec806b101039ff4a2f24c730af6c5bf40218
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-02-24 14:09:56 +0200

    dynamic component demo

--- project-11/src/App.vue
+++ project-11/src/App.vue
@@ -1,39 +1,26 @@
 <template>
   <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-   
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals>
-      <!-- <template v-slot="item"> -->
-      <template #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-        <p>{{ slotProps.anotherProp }}</p>
-      </template>
-    </course-goals>
+    <the-header></the-header> 
 
-    <!-- If only one template aka default, can be moved outside template-->
-    <course-goals #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-        <p>{{ slotProps.anotherProp }}</p>
-    </course-goals>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponents === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponents === 'manage-goals'"></manage-goals> -->
+
+    <component :is="selectedComponents"></component>
   </div>
 </template>
 
 <script>
-import BadgeList from './components/BadgeList.vue';
-import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 import TheHeader from './components/TheHeader.vue';
-import UserInfo from './components/UserInfo.vue';
 
-export default {
-  components: { TheHeader, UserInfo, BadgeList, CourseGoals },
+export default { 
+  components: { TheHeader, ActiveGoals, ManageGoals },
   data() {
     return {
+      selectedComponents: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -41,10 +28,11 @@ export default {
       },
     };
   },
-  mounted() {
-    console.log(this.$attrs)
-  },
-  
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponents = cmp;
+    }
+  }  
 };
 </script>
 
