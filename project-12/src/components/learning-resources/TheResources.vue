82ac20c71a56729f489e83bfde310e3c055d2328
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-02-25 09:23:04 +0200

    adding store resources with injection and TheResources component to clean up app.vue

---
+++ project-12/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <base-card>
+            <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+            <base-button @click="setSelectedTab('add-resources')">Add Resources</base-button>
+        </base-card>
+
+        <component :is="selectedTab"></component>
+    </div>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResources from './AddResources.vue';
+
+export default {
+    components: {
+        StoredResources,
+        AddResources
+    },
+    provide() {
+        return {
+            resources: this.storedResources
+        }
+    },
+    data() {
+        return {
+            selectedTab: 'stored-resources',
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org' 
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to google',
+                    link: 'https://google.org'
+                }
+            ]
+        }
+    },
+    methods: {
+        setSelectedTab(tab) {
+            this.selectedTab = tab
+        }
+    }
+}
+</script>
