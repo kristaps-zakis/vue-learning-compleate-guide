72042bc11306ead5d39c02bc9fcf6e314d32f329
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-02-26 11:50:27 +0200

    adding first routes settings and route-view

--- project-15/src/main.js
+++ project-15/src/main.js
@@ -2,14 +2,25 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamList from './components/teams/TeamsList.vue'
+import UsersList from './components/users/UsersList.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        
-    ],
-});
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/teams',
+      component: TeamList,
+    },
+    {
+      path: '/users',
+      component: UsersList,
+    },
+  ],
+})
 
 const app = createApp(App)
+
+app.use(router)
 
 app.mount('#app');
