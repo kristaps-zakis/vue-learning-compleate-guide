a6052db9baf20cb34f178a2146378f58260d06b6
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-02-26 17:18:28 +0200

    Adding not found and home page redirect

--- project-15/src/main.js
+++ project-15/src/main.js
@@ -5,13 +5,20 @@ import App from './App.vue';
 import TeamList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
 import TeamMembers from './components/teams/TeamMembers.vue'
+import NotFound from './components/nav/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
+      path: '/',
+      // component: TeamList,
+      redirect: '/teams',
+    },
+    {
       path: '/teams',
       component: TeamList,
+      //   alias: '/',
     },
     {
       path: '/teams/:teamId',
@@ -21,6 +28,11 @@ const router = createRouter({
     {
       path: '/users',
       component: UsersList,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound,
+      //   redirect: '/teams',
     },
   ],
   //   linkActiveClass: 'active',
