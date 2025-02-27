a653fe503bfbd31be24d20825f75c6dcd14f6347
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-02-27 12:25:45 +0200

    adding custom router file

---
+++ project-15/src/router.js
@@ -0,0 +1,99 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TeamList from './pages/TeamsList.vue'
+import UsersList from './pages/UsersList.vue'
+import UsersFooter from './components/users/UsersFooter.vue'
+import TeamMembers from './components/teams/TeamMembers.vue'
+import TeamsFooter from './components/teams/TeamsFooter.vue'
+import NotFound from './pages/NotFound.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      {
+        path: '/',
+        // component: TeamList,
+        redirect: '/teams',
+      },
+      {
+        name: 'teams',
+        path: '/teams',
+        meta: {
+          needsAuth: true,
+        },
+        //   component: TeamList,
+        components: {
+          default: TeamList,
+          footer: TeamsFooter,
+        },
+        children: [
+          {
+            name: 'team-members',
+            path: ':teamId',
+            component: TeamMembers,
+            props: true,
+          }, // teams/t1
+        ],
+        //   alias: '/',
+      },
+  
+      {
+        path: '/users',
+        //   component: UsersList,
+        components: {
+          default: UsersList,
+          footer: UsersFooter,
+        },
+        beforeEnter(to, from, next) {
+          console.log('users beforeEnter')
+          console.log(to, from)
+          next()
+        },
+      },
+      {
+        path: '/:notFound(.*)',
+        component: NotFound,
+        //   redirect: '/teams',
+      },
+    ],
+    //   linkActiveClass: 'active',
+    scrollBehavior(to, from, savedPosition) {
+      // to and from accesable also in this.$route
+      // console.log(to, from, savedPosition)
+      if (savedPosition) {
+        return savedPosition
+      }
+      return { left: 0, top: 0 }
+    },
+  })
+  
+  router.beforeEach(function (to, from, next) {
+    console.log('Global beforeEach')
+    console.log(to, from)
+  
+    if (to.meta.needsAuth) {
+      console.log('Needs auth')
+    }
+    //   next(false)
+    next(true)
+    // next('/teams')
+  
+    //   if (to.name === 'team-members') {
+    //     next()
+    //   } else {
+    //     next({
+    //       name: 'team-members',
+    //       params: { teamId: 't3' },
+    //     })
+    //   }
+  })
+  
+  router.afterEach(function (to, from) {
+    console.log('Global afterEach')
+    console.log(to, from)
+  
+    // dont have next
+    // good for analytics
+  })
+
+export default router;
