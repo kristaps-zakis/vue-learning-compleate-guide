32ab67adac436f2a4bc2e35b28b8b1bda23ab3c3
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-03-05 18:29:27 +0200

    creating init form and styling

--- project-19/src/pages/coaches/CoachRegistration.vue
+++ project-19/src/pages/coaches/CoachRegistration.vue
@@ -1,5 +1,21 @@
 <template>
   <section>
+    <base-card>
+  
     <h2>Register</h2>
+    <coach-form></coach-form>
+  </base-card>
   </section>
 </template>
+
+
+<script>
+import CoachForm from '@/components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
+
